e3c753d156431c23aa5c3e971489996c3fa1c250
iGxnon <igxnon@gmail.com>
2023-07-16 00:22:37 +0800

    beautify bar

--- scripts/bar.sh
+++ scripts/bar.sh
@@ -2,7 +2,42 @@
 
 BAT_PATH=/sys/class/power_supply/BAT1
 
+update_vol() {
+  VOL=$(pamixer --get-volume-human)
+}
+
+update_bat() {
+  BAT_CAP=$(cat $BAT_PATH/capacity)
+  BAT_STA=$(cat $BAT_PATH/status)
+}
+
+update_date() {
+  DATE=$(date +"%x %a %H:%M:%S" | awk '{gsub(/ /,",")}1')
+}
+
+update_light() {
+  LIGHT=$(light -G)
+}
+
+update_wifi() {
+  if [ "$(nmcli radio wifi)" == "enabled" ]
+  then
+    WIFI="󰖩 $(nmcli -t -f active,ssid dev wifi | grep -E '^yes' | cut -d: -f2)"
+  else
+    WIFI="󰖪 "
+  fi
+}
+
+display() {
+  xsetroot -name "[ $LIGHT] [󰕾 $VOL] [$WIFI] [󱊣 $BAT_CAP%,$BAT_STA] [$DATE]"
+}
+
 while true; do
-	xsetroot -name "[Light $(light -G)] [Vol $(pamixer --get-volume-human)] [Bat $(cat $BAT_PATH/capacity) $(cat $BAT_PATH/status)] [$(date +"%x %a %H:%M:%S")]"
-	sleep 1
+  update_vol
+  update_bat
+  update_date
+  update_light
+  update_wifi
+  display
+  sleep 1
 done
